fef45005dcd4dfbf6c6e6eb4b2f0e1e6417b3388
bsef19a505 <bsef19a505@pucit.edu.pk>
2022-07-25 00:09:49 +0500

    Menu Class Designing

--- app/src/main/java/com/example/onlinefoodordermobileapp/MainActivity.java
+++ app/src/main/java/com/example/onlinefoodordermobileapp/MainActivity.java
@@ -2,10 +2,18 @@ package com.example.onlinefoodordermobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public void DisplayMenu(View view){
+        Intent intent=new Intent(this,Menu.class);
+        startActivity(intent);
+    }
+    public void PlaceOrder(View view){
 
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
